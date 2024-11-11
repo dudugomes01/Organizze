813346d121a0739954beab24f8eb140caa7376fb
Carlos Eduardo Gomes <dudugomes@MacBook-Air-de-Carlos.local>
2024-11-11 17:04:52 -0300

    feat: add transaction type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,26 @@
-const transactionPage = () => {
-  return <h1>Teste Transaction</h1>;
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+// import AddTransactionButton from "../_components/add-transaction-button";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between p-6">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        {/* <AddTransactionButton /> */}
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} /> 
+    </div>
+  );
 };
 
-export default transactionPage;
+export default TransactionsPage;
