ec9f24152ca880e56ec8a7e31c77475584d19876
Carlos Eduardo Gomes <dudugomes@MacBook-Air-de-Carlos.local>
2024-11-27 13:54:51 -0300

    fix: value saldo

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -1,12 +1,15 @@
+'use client'; 
+
+import React, { useEffect, useState } from 'react';
 import {
   PiggyBankIcon,
   TrendingDownIcon,
   TrendingUpIcon,
   WalletIcon,
-} from "lucide-react";
-import SummaryCard from "./summary-card";
+} from 'lucide-react';
+import SummaryCard from './summary-card';
 
-interface SummaryCards {
+interface SummaryCardsProps {
   month: string;
   balance: number;
   depositsTotal: number;
@@ -15,43 +18,59 @@ interface SummaryCards {
   userCanAddTransaction?: boolean;
 }
 
-const SummaryCards = async ({
+const SummaryCards = ({
   balance,
   depositsTotal,
   expensesTotal,
   investmentsTotal,
   userCanAddTransaction,
-}: SummaryCards) => {
-  return (
-<div className="space-y-6">
-  {/* PRIMEIRO CARD */}
-  <SummaryCard
-    icon={<WalletIcon size={16} />}
-    title="Saldo"
-    amount={balance}
-    size="large"
-    userCanAddTransaction={userCanAddTransaction}
-  />
+}: SummaryCardsProps) => {
+  const [storedBalance, setStoredBalance] = useState<number>(0);
 
-  {/* OUTROS CARDS */}
-  <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-    <SummaryCard
-      icon={<PiggyBankIcon size={16} />}
-      title="Investido"
-      amount={investmentsTotal}
-    />
-    <SummaryCard
-      icon={<TrendingUpIcon size={16} className="text-primary" />}
-      title="Receita"
-      amount={depositsTotal}
-    />
-    <SummaryCard
-      icon={<TrendingDownIcon size={16} className="text-red-500" />}
-      title="Despesas"
-      amount={expensesTotal}
-    />
-  </div>
-</div>
+  useEffect(() => {
+    const savedBalance = localStorage.getItem('balance');
+    if (savedBalance) {
+      setStoredBalance(Number(savedBalance));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (balance > 0) {
+      localStorage.setItem('balance', String(balance));
+      setStoredBalance(balance);
+    }
+  }, [balance]);
+
+  return (
+    <div className="space-y-6">
+      {/* PRIMEIRO CARD */}
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={storedBalance}
+        size="large"
+        userCanAddTransaction={userCanAddTransaction}
+      />
+
+      {/* OUTROS CARDS */}
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investmentsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-red-500" />}
+          title="Despesas"
+          amount={expensesTotal}
+        />
+      </div>
+    </div>
   );
 };
 
