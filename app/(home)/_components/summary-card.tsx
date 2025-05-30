9dd54e21f7a3b94effd24396193cd98554088ea1
Carlos Eduardo Gomes <dudugomes@MacBook-Air-de-Carlos.local>
2025-05-30 18:23:52 -0300

    fix: ajuste page

--- app/(home)/_components/summary-card.tsx
+++ app/(home)/_components/summary-card.tsx
@@ -1,4 +1,59 @@
-import AddTransactionButton from "@/app/_components/add-transaction-button";
+// import AddTransactionButton from "@/app/_components/add-transaction-button";
+// import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+// import { ReactNode } from "react";
+
+// interface SummaryCardProps {
+//   icon: ReactNode;
+//   title: string;
+//   amount: number;
+//   size?: "small" | "large";
+//   userCanAddTransaction?: boolean;
+//   className?: string;
+// }
+
+// const SummaryCard = ({
+//   icon,
+//   title,
+//   amount,
+//   size = "small",
+//   userCanAddTransaction,
+// }: SummaryCardProps) => {
+//   return (
+//     <Card>
+//       <CardHeader className="flex-row items-center gap-4">
+//         {icon}
+//         <p
+//           className={`${
+//             size === "small" ? "text-muted-foreground" : "text-white opacity-70"
+//           }`}
+//         >
+//           {title}
+//         </p>
+//       </CardHeader>
+//       <CardContent className="flex flex-col sm:flex-row justify-between sm:items-center">
+//         <p
+//           className={`font-bold ${size === "small" ? "text-2xl" : "text-4xl"}`}
+//         >
+//           {Intl.NumberFormat("pt-BR", {
+//             style: "currency",
+//             currency: "BRL",
+//           }).format(amount)}
+//         </p>
+
+//         {size === "large" && (
+//           <div className="mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto flex justify-center">
+//             <AddTransactionButton
+//               userCanAddTransaction={userCanAddTransaction}
+//             />
+//           </div>
+//         )}
+//       </CardContent>
+//     </Card>
+//   );
+// };
+
+// export default SummaryCard;
+import AddTransactionButtonWrapper from "@/app/_components/AddTransactionButtonWrapper";
 import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
 import { ReactNode } from "react";
 
@@ -16,7 +71,7 @@ const SummaryCard = ({
   title,
   amount,
   size = "small",
-  userCanAddTransaction,
+  userCanAddTransaction = false,
 }: SummaryCardProps) => {
   return (
     <Card>
@@ -42,7 +97,7 @@ const SummaryCard = ({
 
         {size === "large" && (
           <div className="mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto flex justify-center">
-            <AddTransactionButton
+            <AddTransactionButtonWrapper
               userCanAddTransaction={userCanAddTransaction}
             />
           </div>
